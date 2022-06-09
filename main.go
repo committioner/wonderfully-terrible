8e0c52de7d229d527a44a28098b2a5c9503445d7
coip <e@coip.me>
2022-06-08 21:00:24 -0500

    add ext depends

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+// appdx
+// https://interview-data.herokuapp.com/surveys
+// https://interview-data.herokuapp.com/survey-questions
+// https://interview-data.herokuapp.com/survey-responses
+
 func main() {
 	payload, err := http.Get("https://interview-data.herokuapp.com/companies")
 	if err != nil {
